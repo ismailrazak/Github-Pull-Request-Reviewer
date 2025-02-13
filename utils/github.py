1ce5a97749721cec25ebaadda31d2a2cb67f9895
ismail <ismailrazak009@gmail.com>
2025-02-13 15:13:34 +0530

    update settings

--- utils/github.py
+++ utils/github.py
@@ -8,30 +8,32 @@ from .ai_agent import analyze_content_with_llm
 
 
 def extract_content_from_pr(url, pr_no, token=None):
-    try:
-        headers = {"Authorization": token} if token else {}
-        project = url.split("/")[-1]
-        owner = url.split("/")[-2]
-        pr_response = requests.get(
-            f"https://api.github.com/repos/{owner}/{project}/pulls/{pr_no}/files",
+    # try:
+    headers = {"Authorization": token} if token else {}
+    project = url.split("/")[-1]
+    owner = url.split("/")[-2]
+    pr_response = requests.get(
+        f"https://api.github.com/repos/{owner}/{project}/pulls/{pr_no}/files",
+        headers=headers,
+    )
+    pr_response.raise_for_status()
+    pr_repsonses = pr_response.json()
+    content_list = []
+    for pr_response in pr_repsonses:
+        filename = pr_response.get("filename")
+        content_response = requests.get(
+            f"https://api.github.com/repos/{owner}/{project}/contents/{filename}",
             headers=headers,
         )
-        pr_response.raise_for_status()
-        pr_repsonses = pr_response.json()
-        content_list = []
-        for pr_response in pr_repsonses:
-            filename = pr_response.get("filename")
-            content_response = requests.get(
-                f"https://api.github.com/repos/{owner}/{project}/contents/{filename}",
-                headers=headers,
-            )
-            content_response.raise_for_status()
-            content_base64 = content_response.json().get("content")
-            content = base64.b64decode(content_base64).decode()
-            content_list.append(content)
-        return content_list
-    except requests.exceptions.HTTPError:
-        return []
+        content_response.raise_for_status()
+        content_base64 = content_response.json().get("content")
+        content = base64.b64decode(content_base64).decode()
+        content_list.append(content)
+    return content_list
+
+
+# except requests.exceptions.HTTPError:
+#     return []
 
 
 def analyze_pr(url, pr_no, task_id, token=None):
